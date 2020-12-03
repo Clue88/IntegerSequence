2e37cda0a538efbb00382eafa62255620d7735b4
Christopher Liu <christopherliu@sorcero.com>
2020-12-03 15:48:07 -0500

    Add Range object

---
+++ Range.java
@@ -0,0 +1,27 @@
+import java.util.NoSuchElementException;
+public class Range implements IntegerSequence {
+    private int start, end, current;
+    public Range(int start, int end) {
+        this.start = start;
+        this.end = end;
+        this.current = start;
+    }
+
+    public void reset() {
+        current = start;
+    }
+
+    public int length() {
+        return end - start + 1;
+    }
+
+    public boolean hasNext() {
+        return current != end;
+    }
+
+    public int next() {
+        if (!hasNext()) throw new NoSuchElementException();
+        current += 1;
+        return current - 1;
+    }
+}
