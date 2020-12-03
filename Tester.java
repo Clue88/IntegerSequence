5077f7d8ee5bd58f618a2c2966fee3b686ad4aa2
Christopher Liu <christopherliu@sorcero.com>
2020-12-03 15:59:14 -0500

    Add better tester

--- Tester.java
+++ Tester.java
@@ -1,12 +1,40 @@
+import java.util.ArrayList;
 public class Tester {
+    public static int ERR = 0;
+    public static boolean DEBUG = true;
     public static void main(String[] args) {
-        IntegerSequence r = new Range(10,15);
-        while(r.hasNext()){
-            System.out.print(r.next());
-            if( r.hasNext() ){       
-                System.out.print( ", " );
+        if (args.length > 0 && Boolean.parseBoolean(args[0]) == false) DEBUG = false;
+        String test = "";
+
+        test = "IntegerSequence.hasNext()";
+        try {
+            IntegerSequence r = new Range(10, 15);
+            ArrayList<Integer> a = new ArrayList<Integer>();
+            while (r.hasNext()) {
+                a.add(r.next());
             }
+            check(test, a.toString(), "[10, 11, 12, 13, 14, 15]");
+
+        } catch(RuntimeException e) {
+            except(test, e);
         }
-        System.out.println();
+
+        if (ERR == 0) System.out.println("All good!");
+        else if (ERR == 1) System.out.println("Uh oh... 1 error found.");
+        else System.out.println("Uh oh... " + ERR + " errors found.");
+    }
+
+    public static void check(String test, Object actual, Object expected) {
+        if (!actual.equals(expected)) {
+            System.out.println("Failure on " + test + ": Expected \"" +
+                                expected + "\", got \"" + actual + "\".");
+            ERR++;
+        }
+    }
+
+    public static void except(String test, RuntimeException e) {
+        System.out.println("Failure on " + test + ": RuntimeException");
+        if (DEBUG) System.out.println(e.toString());
+        ERR++;
     }
 }
